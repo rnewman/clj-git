eca2ada9fb79c50a0404e9fef4c0255f2fbe8a30
Richard Newman <Richard Newman  rnewman@franz.com>
2009-07-15 15:19:13 -0700

    First run at a Clojure git client.

---
+++ src/com/twinql/clojure/git.clj
@@ -0,0 +1,89 @@
+(ns com.twinql.clojure.git
+  (:refer-clojure)
+  (:use clojure.contrib.str-utils)
+  (:use clojure.contrib.shell-out))
+
+(defmacro with-repo [repo & body]
+  `(with-sh-dir repo
+     ~@body))
+
+(defn status []
+  (sh "git" "status"))
+
+(defn object-exists? [hash]
+  (= 0 (:exit (sh :return-map true "git" "cat-file" "-e" hash))))
+
+(defn object-size [hash]
+  (Integer/parseInt
+    (chomp
+      (sh "git" "cat-file" "-s" hash))))
+
+(defn object-type [hash]
+  (chomp
+    (sh "git" "cat-file" "-t" hash)))
+  
+(defn cat-object [hash]
+  "Returns the contents as a string."
+  (sh "git" "cat-file" "blob" hash))
+    
+(defn hash-object-from-string
+  "Returns the SHA-1."
+  [object path write? filters?]
+  (chomp
+    (apply sh
+           :in object
+           (concat
+             ["git" "hash-object" "--stdin"]
+             (when path ["--path" path])
+             (when write? ["-w"])
+             (when (not filters?) ["--no-filters"])))))
+
+(defn make-tree
+  "Each blob is a pair of SHA1, name."
+  [blobs]
+  (chomp
+    (sh :in (str
+              (map (fn [[sha1 name]]
+                     (str "100644 " sha1 \tab name \newline))
+                   blobs))
+        "git" "mktree")))
+
+(defn reverse-line-map
+  "Take a string consisting of space-separated values, returning a map from the second half to the first."
+  [#^String lines]
+  (letfn [(line->map-entry [#^String line]
+            (let [space (int (.indexOf line (int \space)))]
+              (when space
+                [(.substring line (+ 1 space))
+                 (.substring line 0 space)])))]
+    (with-open [s (java.io.StringReader. lines)
+                b (java.io.BufferedReader. s)]
+      (into {} (seq (map line->map-entry (line-seq b)))))))
+
+(defn refs->commits []
+  (reverse-line-map
+    (sh "git" "show-ref")))
+
+(defn ref->commit
+  "Takes a full refspec, such as \"refs/heads/master\"."
+  [ref]
+  ((reverse-line-map
+     (sh "git" "show-ref" ref))
+     ref))
+    
+(defn commit-tree
+  [tree parent author committer message]
+  (chomp
+    (apply sh
+           (concat
+             [:in message
+              :env {"GIT_AUTHOR_NAME" author
+                    "GIT_COMMITTER_NAME" committer}
+              "git" "commit-tree"]
+             (when parent ["-p" parent])
+             [tree]))))
+
+(defn update-ref
+  [ref commit]
+  (chomp
+    (sh "git" "update-ref" ref commit)))
